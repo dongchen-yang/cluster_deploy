868b2b960a908bcae48e5f8c9df3117183d22e5f
dyang111 <48226450+dyang111@users.noreply.github.com>
2021-10-21 17:24:23 -0700

    Add files via upload

---
+++ NYU.sh
@@ -0,0 +1,12 @@
+
+
+pip3 install --upgrade pip
+pip3 install opencv-python
+apt-get update
+apt-get install ffmpeg libsm6 libxext6  -y
+pip3 install visdom dominate 
+
+
+cd /eccv20dataset/dy/dcy
+
+python3 train.py --batchSize=6 
