12cdfe68b276b26455d65aa90b2ee9a8726b0554
dyang111 <48226450+dyang111@users.noreply.github.com>
2021-10-22 01:37:44 -0700

    Update NYU.sh

--- NYU.sh
+++ NYU.sh
@@ -1,12 +1,8 @@
-
-
+#!/bin/sh
 pip3 install --upgrade pip
 pip3 install opencv-python
 apt-get update ##asdfasdf
 apt-get install ffmpeg libsm6 libxext6  -y
 pip3 install visdom dominate 
-
-
 cd /eccv20dataset/dy/dcy
-
 python3 train.py --batchSize=16
