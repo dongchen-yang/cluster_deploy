570197322f3df5181fce36acb3b72fbee82a3beb
dyang111 <yangdongchen1@gmail.com>
2021-10-22 04:11:43 -0700

    new sh

---
+++ NYU_all_task.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+pip3 install --upgrade pip
+pip3 install opencv-python
+apt-get update ##asdfasdf
+apt-get install ffmpeg libsm6 libxext6  -y
+pip3 install visdom dominate 
+cd /openroomssubstance/dcy/dcy_all
+python3 train.py --batchSize=12
