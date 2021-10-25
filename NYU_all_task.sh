7fe726bc92a006b7a2111e3ccc56448fae067c43
dyang111 <48226450+dyang111@users.noreply.github.com>
2021-10-25 01:13:31 -0700

    Update NYU_all_task.sh

--- NYU_all_task.sh
+++ NYU_all_task.sh
@@ -6,4 +6,4 @@ apt-get update ##asdfasdf
 apt-get install ffmpeg libsm6 libxext6  -y
 pip3 install visdom dominate 
 cd /openroomssubstance/dcy/dcy_all
-python3 train.py --batchSize=12
+python3 train.py --batchSize=12 --name tune-w1
