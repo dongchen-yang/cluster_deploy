bd674b6865470bb8af53cbfaea0e05f1367db292
dyang111 <48226450+dyang111@users.noreply.github.com>
2021-10-28 16:15:41 -0700

    Update NYU.sh

--- NYU.sh
+++ NYU.sh
@@ -5,5 +5,5 @@ pip3 install opencv-python
 apt-get update ##asdfasdf
 apt-get install ffmpeg libsm6 libxext6  -y
 pip3 install visdom dominate 
-cd /eccv20dataset/dy/dcy
-python3 train.py --batchSize=16
+cd /eccv20dataset/dy/Synthetic2Realistic
+python3 script_trainDA.py
