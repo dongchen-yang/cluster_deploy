30d67fd2b3780ae5a5f0c3bc9156ed94a14e7361
dyang111 <48226450+dyang111@users.noreply.github.com>
2021-11-10 03:14:41 -0800

    Update IIW_albedo_depth.sh

--- IIW_albedo_depth.sh
+++ IIW_albedo_depth.sh
@@ -5,5 +5,5 @@ pip3 install opencv-python
 apt-get update ##asdfasdf
 apt-get install ffmpeg libsm6 libxext6  -y
 pip3 install visdom dominate 
-cd /eccv20dataset/dy/Synthetic2Realistic/script_train
-python3 script_trainDA_IIW_albedo_depth.py
+cd /eccv20dataset/dy/Synthetic2Realistic
+python3 script_train/script_trainDA_IIW_albedo_depth.py
