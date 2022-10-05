61d24f75e1704d920ae1698f5f85b3820de80e90
dyang111 <48226450+dyang111@users.noreply.github.com>
2022-10-05 01:01:34 -0700

    Update run_trainSVBRDF.sh

--- run_trainSVBRDF.sh
+++ run_trainSVBRDF.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+bash /openroomssubstance/dcy/Miniconda3-py39_4.12.0-Linux-x86_64.sh -b
+export "PATH=~/miniconda3/bin:$PATH"
 pip3 install --upgrade pip
 pip3 install opencv-python
 apt-get update ##asdfasdf
 apt-get install ffmpeg libsm6 libxext6  -y
 pip3 install visdom dominate 
 cd /openroomssubstance/dcy/InverseRenderingOfIndoorScene/
-source /openroomssubstance/dcy/InverseRenderingOfIndoorScene/setup_conda.sh
 python3 train_script.py
